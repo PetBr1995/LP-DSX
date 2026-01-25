f9db78d840e477bab7a12f95663465c7cf367c4a
PetBr1995 <peterson.albuq@gmail.com>
2026-01-24 22:56:45 -0400

    Adição da CallToAction de patrocinadores

--- src/pages/Vendas.jsx
+++ src/pages/Vendas.jsx
@@ -1,12 +1,13 @@
 import HeroVendas from "../components/ComponentsVendas/HeroVendas";
 import SecondSectionVendas from "../components/ComponentsVendas/SecondSectionVendas";
-
+import CallToActionPatrocinadores from "../components/ComponentsPatrocinadores/CallToActionPatrocinadores";
 const Vendas = () => {
   return (
     <>
       <section className="overflow-x-hidden">
         <HeroVendas />
         <SecondSectionVendas />
+        <CallToActionPatrocinadores />
       </section>
     </>
   );
