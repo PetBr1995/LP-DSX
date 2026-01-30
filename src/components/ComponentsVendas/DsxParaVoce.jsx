80415102ae6a9e89df3a0c2972dbd63c94f5e8ca
PetBr1995 <peterson.albuq@gmail.com>
2026-01-30 16:13:02 -0400

    Ajuste na seção de Dsx é para vocÊ da pagina de vendas

--- src/components/ComponentsVendas/DsxParaVoce.jsx
+++ src/components/ComponentsVendas/DsxParaVoce.jsx
@@ -30,64 +30,70 @@ const DsxParaVoce = () => {
   return (
     <section
       className="
-  py-10 relative overflow-hidden
-  after:absolute after:inset-0
-  after:content-['']
-  after:bg-[url(/banner-dsx-para-voce.png)]
-  after:bg-cover after:bg-center after:bg-no-repeat
-  after:z-0
-  after:brightness-75
-"
+        py-10 relative overflow-hidden
+        after:absolute after:inset-0
+        after:content-['']
+        after:bg-[url(/banner-dsx-para-voce.png)]
+        after:bg-cover after:bg-center after:bg-no-repeat
+        after:z-0
+        after:brightness-75
+      "
     >
-      {/* Título */}
       <h2 className="text-white relative z-10 font-anton uppercase text-4xl sm:text-5xl md:text-6xl text-center">
         O DSX é para você…
       </h2>
 
       <div className="relative z-10 max-w-7xl mx-auto mt-10 px-4 flex flex-col gap-6">
         {/* LINHA 1 */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 place-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 items-stretch place-items-center">
           {infCard1.map((inf, index) => (
             <div
               key={index}
               className="
                 border border-white rounded-md
                 p-6
-                flex flex-col justify-center gap-4
+                flex flex-col gap-4
                 bg-black/40 backdrop-blur-sm
                 w-full max-w-[380px]
-                min-h-[190px]
+                h-full
+                items-center text-center justify-center
+                md:items-start md:text-left md:justify-start
               "
             >
-              <img src={inf.icon} alt="icon" className="w-15 h-15" />
-
-              <p className="text-white text-xl leading-relaxed">{inf.desc}</p>
+              <img src={inf.icon} alt="icon" className="w-14 h-14" />
+              <p className="text-white text-xl leading-relaxed">
+                {inf.desc}
+              </p>
             </div>
           ))}
         </div>
 
         {/* LINHA 2 */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 place-items-center max-w-[820px] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch place-items-center max-w-[820px] mx-auto">
           {infCard2.map((inf, index) => (
             <div
               key={index}
               className="
                 border border-white rounded-md
                 p-6
-                flex flex-col justify-center gap-4
+                flex flex-col gap-4
                 bg-black/40 backdrop-blur-sm
                 w-full max-w-[380px]
-                min-h-[190px]
+                h-full
+                items-center text-center justify-center
+                md:items-start md:text-left md:justify-start
               "
             >
-              <img src={inf.icon} alt="icon" className="w-15 h-15" />
-
-              <p className="text-white text-xl leading-relaxed">{inf.desc}</p>
+              <img src={inf.icon} alt="icon" className="w-14 h-14" />
+              <p className="text-white text-xl leading-relaxed">
+                {inf.desc}
+              </p>
             </div>
           ))}
         </div>
-        <div className="py-8">
-          <CTAButton titulo="Quero meu passaporte" link="#"/>
+
+        <div className="py-8 flex justify-center">
+          <CTAButton titulo="Quero meu passaporte" link="#" />
         </div>
       </div>
     </section>
