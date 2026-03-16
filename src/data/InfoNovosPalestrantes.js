35f8990735f169da0aa941b945dad56ac9e9911a
PetBr1995 <dev.petersonbrito@gmail.com>
2026-03-16 10:03:15 -0400

    Ajustes

--- src/data/InfoNovosPalestrantes.js
+++ src/data/InfoNovosPalestrantes.js
@@ -3,16 +3,19 @@ export const InfoNovosPalestrantes = [
     nome: "Fernando Miranda",
     desc: "CEO da Staage",
     img: "/FERNANDO-MIRANDA-FOTO.png",
+    badge: "Nacional",
   },
   {
     nome: "Nicolas Charão",
     desc: " Founder Grupo VTX",
     img: "/foto-nicolas-charao.jpg",
+    badge: "Nacional",
   },
   {
     nome: "Roberto Reis",
     desc: "Estrategista eleitoral e analista político",
     img: "/foto-roberto-reis-3.png",
+    badge: "Nacional",
   },
   /*
    */
@@ -21,17 +24,20 @@ export const InfoNovosPalestrantes = [
     desc: "Investidor anjo, empreendedor e autor best-seller.",
     img: "/foto-kepler.jpg",
     bgSize: "155%",
-    bgPosition: "center -70px",
+    bgPosition: "center -60px",
+    badge: "Nacional",
   },
   {
     nome: "Flávia Soares  ",
     desc: "Referência em pesquisa qualitativa.",
     img: "/foto-flavia.jpg",
+    badge: "Local",
   },
   {
     nome: "Afrânio Soares",
     desc: "Referência regional em inteligência de mercado.",
     img: "/foto-afranio.jpg",
+    badge: "Local",
   },
   {
     nome: "",
